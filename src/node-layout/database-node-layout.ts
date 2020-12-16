722b10324613db1191cabf745e51ea335abe0574
Michiel Overeem <overeemm@gmail.com>
2020-12-16 08:02:56 +0100

    Added database node type

---
+++ src/node-layout/database-node-layout.ts
@@ -0,0 +1,74 @@
+import { NodeData, NodePosition } from '../api';
+import { XY } from '../interfaces';
+import { NodeDimensions, NodeState, RenderedNode } from './node-common';
+import { h } from 'maquette';
+
+const ellipseDiameterWidth = 40;
+const ellipseDiameterHeight = 10;
+
+export function renderDatabaseNodeLayout(
+  data: NodeData,
+  position: NodePosition,
+  dragPosition: XY | undefined,
+  mouseDownEventHandler: (evt: MouseEvent) => void
+): RenderedNode {
+  let center: XY = dragPosition ?? position;
+
+  let dimensions: NodeDimensions = {
+    center,
+    left: center.x - ellipseDiameterWidth,
+    right: center.x + ellipseDiameterWidth,
+    bottom: center.y + ellipseDiameterWidth,
+    top: center.y - ellipseDiameterWidth,
+    height: ellipseDiameterHeight * 6, // the rect plus twice the ellipse diameter height
+    width: ellipseDiameterWidth * 2
+  };
+
+  return {
+    dimensions,
+    vNode: h('g',
+      {
+        key: position,
+        cursor: 'move',
+        transform: `translate(${dimensions.left},${dimensions.top})`,
+        'data-nodetype': data.typeName,
+        onmousedown: mouseDownEventHandler
+      },
+      [
+        h('title', { id: 'unique-id' }, [data.displayName]),
+        h('rect', {
+          width: dimensions.width,
+          height: ellipseDiameterHeight * 4,
+          y: ellipseDiameterHeight * 2,
+          fill: 'white',
+          stroke: 'black',
+          filter: 'none',
+        }),
+        h('ellipse', {
+          key: data,
+          cx: ellipseDiameterWidth,
+          cy: ellipseDiameterHeight * 2,
+          rx: ellipseDiameterWidth,
+          ry: ellipseDiameterHeight,
+          fill: 'white',
+          stroke: 'black',
+          'stroke-width': 1,
+          title: data.displayName,
+          onmousedown: mouseDownEventHandler
+        }),
+        h('ellipse', {
+          key: data,
+          cx: ellipseDiameterWidth,
+          cy: ellipseDiameterWidth + (ellipseDiameterHeight * 2),
+          rx: ellipseDiameterWidth,
+          ry: ellipseDiameterHeight,
+          fill: 'white',
+          stroke: 'black',
+          'stroke-width': 1,
+          title: data.displayName,
+          onmousedown: mouseDownEventHandler
+        })
+      ]
+    )
+  };
+}
--- src/node-layout/node-factory.ts
+++ src/node-layout/node-factory.ts
@@ -4,6 +4,7 @@ import { NodeState } from './node-common';
 import { renderSmallCircleNodeLayout } from './small-circle-node-layout';
 import { renderDefaultNodeLayout } from './default-node-layout';
 import { renderBigCircleNodeLayout } from './big-circle-node-layout';
+import { renderDatabaseNodeLayout } from './database-node-layout';
 
 export function renderNode(
   data: NodeData,
@@ -18,6 +19,8 @@ export function renderNode(
         return renderBigCircleNodeLayout(data, position, dragPosition, mouseDownEventHandler);
       case 'small-circle':
         return renderSmallCircleNodeLayout(data, position, dragPosition, mouseDownEventHandler);
+        case 'database':
+        return renderDatabaseNodeLayout(data, position, dragPosition, mouseDownEventHandler);
       default:
         return renderDefaultNodeLayout(data, position, dragPosition, mouseDownEventHandler);
     }
